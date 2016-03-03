1248b567203316ffc036e8736632b9ef633223fb
const3 <Sam@MacBook-Air.local>
2016-03-03 16:47:49 -0600

    Se hace prueba de lista de libros

--- js/populate.js
+++ js/populate.js
@@ -2,6 +2,8 @@ $(document).ready(function(e){
   var items="";
   $.getJSON("../prueba.php",function(data){
   	console.log(data);
+  	$("#kl").html(data); 
+
     $.each(data,function(index,item) 
     {
       items+="<li>"+item.nombre+"</li>";
