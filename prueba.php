273abee016d3fa9a582ba6682cf266e5e9eb3236
const3 <samuel.9401@gmail.com>
2016-03-03 16:55:34 -0600

    Se hace prueba de lista de libros

--- prueba.php
+++ prueba.php
@@ -2,9 +2,9 @@
 include('connect.php'); 
    $db = pg_connect( "$host $port $dbname $credentials"  );
    if(!$db){
-      echo "Error : Unable to open database\n";
+      //echo "Error : Unable to open database\n";
    } else {
-      echo "Opened database successfully\n";
+      //echo "Opened database successfully\n";
    }
 
    $sql =<<<EOF
