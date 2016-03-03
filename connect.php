239f5cda95c211653d2bac769db0b51e7c27ae1f
const3 <Sam@MacBook-Air.local>
2016-03-03 15:06:17 -0600

    Se agreo conexion

---
+++ connect.php
@@ -0,0 +1,7 @@
+<?php
+# FileName="connect.php"
+$host        = "host=ec2-54-225-194-162.compute-1.amazonaws.com";
+$port        = "port=5432";
+$dbname      = "dbname=d3lk5c6d4il4pe";
+$credentials = "user=ejeujmerbxnesi password=i1RSSLCwr672ZSnB-qyuKupbYY";
+?>
