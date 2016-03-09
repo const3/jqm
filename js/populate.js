d1ad0f5c4b3ffdecbc87a0b2b0a47071e377b840
const3 <samuel.9401@gmail.com>
2016-03-09 15:00:25 -0600

    Se configuro form

--- js/populate.js
+++ js/populate.js
@@ -9,7 +9,7 @@ $(document).ready(function(e){
     {
       items+="<li><a href=\"#\">"+item.nombre+"</a></li>";
     });
-    $("#good").append(items).listview('refresh');; 
+    $("#good").append(items).listview('refresh');
   });
 }
 
@@ -19,15 +19,16 @@ $(document).ready(function(e){
     //$("#result").html('');
     var values = $(this).serialize();
     console.log(values);
- //       ajaxRequest= $.ajax({
- //            url: "test.php",
- //            type: "post",
- //            data: values
- //        });
- //     ajaxRequest.done(function (response, textStatus, jqXHR){
- //          $("#result").html('Submitted successfully');
- //     });
- //     ajaxRequest.fail(function (){
- //       $("#result").html('There is error while submit');
+       ajaxRequest= $.ajax({
+            url: "../insert.php",
+            type: "post",
+            data: values
+        });
+     ajaxRequest.done(function (response, textStatus, jqXHR){
+      $("#good").html('');
+      getLibros();
+     });
+     ajaxRequest.fail(function (){
+       $("#result").html('There is error while submit');
 	 });
 });
