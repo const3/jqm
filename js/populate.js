40f9c7b51a3eb833e2893f260adde04ea8726888
const3 <samuel.9401@gmail.com>
2016-03-09 16:48:15 -0600

    Se configuro form

--- js/populate.js
+++ js/populate.js
@@ -28,14 +28,28 @@ $(document).ready(function(e){
         getLibros();
         $('#formLibro').trigger("reset");
         $('#myPopupDialog').popup('close');  
-        $('#dialog').popup('open', {history: false});
-        // setTimeout(function(){
-        //   $("#dialog").popup('close')
-        // }, 3000);
+        setTimeout(function(){
+              $("#dialog").popup();
+              $("#dialog").popup("open");
+            }, 100);
+            setTimeout(function(){
+              $("#dialog").popup("close");
+            }, 5000);
+
       });
       ajaxRequest.fail(function (){
         $("#result").html('There is error while submit');
   	  });
     });
+
+     $( '#pageone' ).bind( 'pageinit',function(event){
+            setTimeout(function(){
+              $("#dialog").popup();
+              $("#dialog").popup("open");
+            }, 100);
+            setTimeout(function(){
+              $("#dialog").popup("close");
+            }, 5000);
+          });
 
 });
