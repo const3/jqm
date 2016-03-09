14ff462c3a558783c01d423ca473bd05ec55eae4
const3 <samuel.9401@gmail.com>
2016-03-09 14:32:08 -0600

    Se configuro form

--- js/populate.js
+++ js/populate.js
@@ -7,4 +7,22 @@ $(document).ready(function(e){
     });
     $("#good").append(items).listview('refresh');; 
   });
+
+  $( "#formLibro" ).submit(function( event ) {
+  	 //var ajaxRequest;
+    event.preventDefault();
+    //$("#result").html('');
+    var values = $(this).serialize();
+    console.log(values);
+ //       ajaxRequest= $.ajax({
+ //            url: "test.php",
+ //            type: "post",
+ //            data: values
+ //        });
+ //     ajaxRequest.done(function (response, textStatus, jqXHR){
+ //          $("#result").html('Submitted successfully');
+ //     });
+ //     ajaxRequest.fail(function (){
+ //       $("#result").html('There is error while submit');
+	// });
 });
